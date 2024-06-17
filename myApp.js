ad103f2592c7940964263ce22ada5a9f4e58e832
wow <150044554+thisissomethingwow@users.noreply.github.com>
2024-06-17 15:55:41 -0230

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,4 +1,3 @@
-
 global.DEBUG = true;
 const fs = require("fs");
 
@@ -38,40 +37,3 @@ switch(myArgs[0]){
     })
         break;
 }
-=======
-global.DEBUG = true;
-const fs = require("fs");
-
-const myArgs = process.argv.slice(2);
-
-const {initApp} = require("./init.js")
-const {configApp} = require("./config.js")
-
-
-
-if(DEBUG) if(myArgs.length >= 1) console.log('myArgs: ', myArgs);
-
-switch(myArgs[0]){
-    case 'init':
-    case 'i':
-        if(DEBUG) console.log("this is the init")
-            initApp();
-        break;
-    case 'config':
-        if (DEBUG) console.log("this is the config")
-            configApp();
-        break;
-    case 'c':
-        if(DEBUG) console.log("this is the config")
-        break;
-    case 'help':
-    case 'h':
-    default:
-        if(DEBUG) console.log("this is the help") 
-        fs.readFile(__dirname+"/help.txt",(error,data)=>{
-            if(error)throw error;
-            console.log(data.toString());
-    })
-        break;
-}
-
