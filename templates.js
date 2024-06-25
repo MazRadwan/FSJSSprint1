2ad5703ab819677cc3e4ac36a1427b28912ffed7
Maz Radwan <maz.radwan@keyin.com>
2024-06-25 11:08:42 -0230

    deprecate code

--- templates.js
+++ templates.js
@@ -18,38 +18,6 @@ const tokenTempl = {
   expires: "",
 };
 
-// const ejs = `<!doctype html>
-// <html>
-// <body>
-//     <style>
-//         body{
-//             text-align: center;
-//             background-color: burlywood;
-
-//         }
-//         input{
-//             background-color: aquamarine;
-//             margin-bottom: 10px;
-//             border-radius: 5px;
-//         }
-//         button{
-//             background-color: rebeccapurple;
-//             color: white;
-//             border-radius: 20px;
-//         }
-
-//     </style>
-//     <h1>
-//     Add a new user token:<br />
-// </h1>
-//     <form action="/" method="post">
-//         <input type="text" name="username" /><br />
-//         <button>Save</button>
-//     </form>
-
-// </body>
-// </html>`;
-
 module.exports = {
   folders,
   configjson,
